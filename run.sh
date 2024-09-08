2ced4e20bd26f21422d29a543ee1671b440f8901
Landon Carter <lcarter1239@gmail.com>
2024-09-07 23:25:59 -0400

    fix GET endpoint

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-AWS_ACCESS_KEY_ID=<set me>
-AWS_SECRET_ACCESS_KEY=<set me>
+AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
+AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
 
 npx node-git-lfs
